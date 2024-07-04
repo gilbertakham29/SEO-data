d54a83d3105c033e9b0d3023c0b3f4e422d57054
Gilbert <akhamgilbert@gmail.com>
2024-07-04 18:29:11 +0530

    change in port number

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import bodyParser from 'body-parser';
 const app = express();
-const port = 3000;
+const port = 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/api', (req, res) => {
